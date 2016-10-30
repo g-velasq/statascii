71551c0b9ff231e4ef53f621d1e3eb661d7834ed
gvelasq2 <gvelasq2@users.noreply.github.com>
2016-10-30 11:00:13 +0000

    Update statascii.R

--- R/statascii.R
+++ R/statascii.R
@@ -9,69 +9,69 @@ library(dplyr)
 library(stringr)
 
 tbl_wrap <- function(tbl, M = M, M1 = M1, width = getOption("width")) {
-  stopifnot(is.matrix(tbl))
-  if (max(nchar(tbl)) <= width) {
-    cat(tbl, sep = "\n")
-  }
-  if (max(nchar(tbl)) > width) {
-    M_rest <- M[-1]+3L
-    M_rest[1] <- M_rest[1]-1L
-    M_start <- M[-1]
-    M_start[seq_along(M_start)] <- 0L
-    M_start[1] <- 1L
-    M_end <- M[-1]
-    M_end[seq_along(M_end)] <- 0L
-    M_end[1] <- M_rest[1]
-    if (length(M_rest) > 1L) {
-      for (i in 2:length(M_rest)) {
-        M_end[i] <- M_end[i-1L] + M_rest[i]
-        M_start[i] <- M_end[i-1L] + 1L
-      }
+    stopifnot(is.matrix(tbl))
+    if (max(nchar(tbl)) <= width) {
+        cat(tbl, sep = "\n")
     }
-    col1 <- as.matrix(str_sub(tbl, start = 1L, end = M1[1]+4L))
-    col_rest <- as.matrix(str_sub(tbl, start = M1[1]+5L, end = -1L))
-    pos <- matrix(c(M_start, M_end), ncol = 2)
-    all_cols <- list()
-    if (length(M_rest) > 1L) {
-      for (i in 1:length(M_rest)) {
-        all_cols[[i+1L]] <- as.matrix(str_sub(col_rest, pos[i,1], pos[i,2]))
-      }
+    if (max(nchar(tbl)) > width) {
+        M_rest <- M[-1]+3L
+        M_rest[1] <- M_rest[1]-1L
+        M_start <- M[-1]
+        M_start[seq_along(M_start)] <- 0L
+        M_start[1] <- 1L
+        M_end <- M[-1]
+        M_end[seq_along(M_end)] <- 0L
+        M_end[1] <- M_rest[1]
+        if (length(M_rest) > 1L) {
+            for (i in 2:length(M_rest)) {
+                M_end[i] <- M_end[i-1L] + M_rest[i]
+                M_start[i] <- M_end[i-1L] + 1L
+            }
+        }
+        col1 <- as.matrix(str_sub(tbl, start = 1L, end = M1[1]+4L))
+        col_rest <- as.matrix(str_sub(tbl, start = M1[1]+5L, end = -1L))
+        pos <- matrix(c(M_start, M_end), ncol = 2)
+        all_cols <- list()
+        if (length(M_rest) > 1L) {
+            for (i in 1:length(M_rest)) {
+                all_cols[[i+1L]] <- as.matrix(str_sub(col_rest, pos[i,1], pos[i,2]))
+            }
+        }
+        all_cols[[1]] <- col1
+        col_widths <- vector(mode = "integer", length = length(all_cols))
+        col_sums <- vector(mode = "integer", length = length(all_cols))
+        col_index <- vector(mode = "integer", length = length(all_cols))
+        wrap_count <- 1L
+        for (i in 1:length(all_cols)) {
+            col_widths[i] <- max(nchar(all_cols[[i]]))
+            col_index[i] <- wrap_count
+        }
+        col_sums[1L] <- col_widths[1L]
+        for (i in 2:length(all_cols)) {
+            col_sums[i] <- col_widths[i] + col_sums[i-1L]
+            if (col_sums[i] > width) {
+                wrap_count <- wrap_count + 1L
+                col_sums[i] <- col_widths[1L] + col_widths[i]
+            }
+            if (wrap_count > 1L) {
+                col_index[i] <- wrap_count
+            }
+        }
+        wrapped <- vector(mode = "list", length = wrap_count)
+        for (i in 1:length(wrapped)) {
+            wrapped[i] <- all_cols[1]
+        }
+        for (j in 2:length(col_index)) {
+            current_list <- col_index[j]
+            wrapped[[current_list]] <- as.matrix(paste0(as.matrix(unlist(wrapped[current_list])), as.matrix(unlist(all_cols[j]))))
+        }
+        for (i in 1:length(wrapped)) {
+            cat(wrapped[[i]], sep = "\n")
+            if (i < length(wrapped)) {
+                cat("\n")
+            }
+        }
     }
-    all_cols[[1]] <- col1
-    col_widths <- vector(mode = "integer", length = length(all_cols))
-    col_sums <- vector(mode = "integer", length = length(all_cols))
-    col_index <- vector(mode = "integer", length = length(all_cols))
-    wrap_count <- 1L
-    for (i in 1:length(all_cols)) {
-      col_widths[i] <- max(nchar(all_cols[[i]]))
-      col_index[i] <- wrap_count
-    }
-    col_sums[1L] <- col_widths[1L]
-    for (i in 2:length(all_cols)) {
-      col_sums[i] <- col_widths[i] + col_sums[i-1L]
-      if (col_sums[i] > width) {
-        wrap_count <- wrap_count + 1L
-        col_sums[i] <- col_widths[1L] + col_widths[i]
-      }
-      if (wrap_count > 1L) {
-        col_index[i] <- wrap_count
-      }
-    }
-    wrapped <- vector(mode = "list", length = wrap_count)
-    for (i in 1:length(wrapped)) {
-      wrapped[i] <- all_cols[1]
-    }
-    for (j in 2:length(col_index)) {
-      current_list <- col_index[j]
-      wrapped[[current_list]] <- as.matrix(paste0(as.matrix(unlist(wrapped[current_list])), as.matrix(unlist(all_cols[j]))))
-    }
-    for (i in 1:length(wrapped)) {
-      cat(wrapped[[i]], sep = "\n")
-      if (i < length(wrapped)) {
-        cat("\n")
-      }
-    }
-  }
 }
 
 statascii <- function(df, flavor = "oneway", padding = "stata", pad = 1L, ...) {
